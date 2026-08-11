792d5bee54f049aeaa3bc94eabdb3a58630a1a17
srinivasanb2004 <bsrinivasan2004@gmail.com>
2026-08-11 11:14:48 +0530

    Final DOB Updated

--- app/dashboard/students/new/page.tsx
+++ app/dashboard/students/new/page.tsx
@@ -81,7 +81,7 @@ export default function AddStudentPage() {
           </select>
 
           <div className="flex flex-col gap-2 w-full">
-            <label className="text-sm font-medium text-gray-700">
+            <label className="block md:hidden text-sm font-medium text-gray-700">
               Date of Birth
             </label>
 
